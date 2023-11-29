a92c20503685922b371ef646636dbcf9e4da62b9
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-11-29 23:35:05 +0700

    transaction order page

---
+++ MagicPost-fe/src/components/table/TransactionOrderTable.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { PencilLine, Trash2 } from 'lucide-svelte';
+	import type { TableTransactionOrderInteface } from 'src/utils/interface';
+
+	export let tableData: TableTransactionOrderInteface[] = [];
+</script>
+
+<div class="table-container !rounded-none h-full">
+	<table class="table table-hover overflow-auto !bg-transparent !rounded-none">
+		<thead class="!bg-white">
+			<tr>
+				<th>STT</th>
+				<th>Mã đơn hàng</th>
+				<th>Điểm giao dịch</th>
+				<th>Mã đơn giao dịch</th>
+				<th>Thao tác</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each tableData as row, i}
+				<tr>
+					<td>{i + 1}</td>
+					<td>{row.orderId}</td>
+					<td>{row.address}</td>
+					<td>{row.transactionId}</td>
+					<td class="flex items-center gap-3">
+						<button type="button" class="btn-icon variant-filled h-8 w-8"><PencilLine size="16" /></button>
+						<button type="button" class="btn-icon variant-filled h-8 w-8"><Trash2 size="16" /></button>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot />
+	</table>
+</div>
+
+<style>
+	thead th {
+		position: sticky;
+		top: 0;
+		background-color: #fff;
+		text-transform: none;
+	}
+
+	.table tbody td {
+		padding-left: 1rem;
+		vertical-align: middle;
+	}
+</style>
---
+++ MagicPost-fe/src/routes/manage/transaction-order/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { PlusCircle } from 'lucide-svelte';
+	import TableTransactionOrder from 'src/components/table/TransactionOrderTable.svelte';
+	import type { TableTransactionOrderInteface } from 'src/utils/interface';
+
+    const tableData: TableTransactionOrderInteface[] = [
+		{ orderId: '1', address: '144  Xuân Thủy', transactionId: '1' },
+		{ orderId: '2', address: '144  Xuân Thủy', transactionId: '2' },
+		{ orderId: '3', address: '144  Xuân Thủy', transactionId: '3' },
+		{ orderId: '4', address: '144  Xuân Thủy', transactionId: '4' },
+		{ orderId: '5', address: '144  Xuân Thủy', transactionId: '5' },
+		{ orderId: '6', address: '144  Xuân Thủy', transactionId: '6' },
+		{ orderId: '7', address: '144  Xuân Thủy', transactionId: '7' },
+		{ orderId: '8', address: '144  Xuân Thủy', transactionId: '8' },
+		{ orderId: '9', address: '144  Xuân Thủy', transactionId: '9' },
+		{ orderId: '10', address: '144  Xuân Thủy', transactionId: '10' },
+	];
+</script>
+
+<main class="h-full">
+    <div class="flex justify-between items-center mb-3">
+		<h1 class="h3 uppercase">Danh sách đơn giao dịch</h1>
+		<button class="btn variant-filled bg-ocean">
+			<PlusCircle class="mr-1" size="20" /> Thêm mới
+		</button>
+	</div>
+    <div class="card py-3 px-4 mb-3">
+		<span class="mr-2">Tìm kiếm</span>
+		<input type="text" placeholder="Nhập tên, mã đơn hàng..." class="dui-input dui-input-bordered w-full max-w-xs" />
+	</div>
+	<div class="card !rounded-none h-[calc(100%-7.5rem)]">
+		<TableTransactionOrder tableData={tableData} />
+	</div>
+</main>
