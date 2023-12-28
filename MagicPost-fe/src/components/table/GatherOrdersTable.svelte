d932b89b31291943c2732f97d045e59c4adc8df7
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-12-29 00:34:31 +0700

    gathering order UI

--- MagicPost-fe/src/components/table/GatherOrdersTable.svelte
+++ MagicPost-fe/src/components/table/GatherOrdersTable.svelte
@@ -1,25 +1,60 @@
 <script lang="ts">
-	import { PencilLine, Trash2 } from 'lucide-svelte';
+	import { ArrowRightFromLine, ClipboardCheck, Eye, PencilLine, Trash2 } from 'lucide-svelte';
 	import EmptyData from '../EmptyData.svelte';
 	import OfficeModal from '../modal/OfficeModal.svelte';
 	import DeleteConfirmModal from '../modal/DeleteConfirmModal.svelte';
-	import type { GatherOrderInteface } from 'src/utils/interface';
+	import type { Order, GatherOrderInteface } from 'src/utils/interface';
 
-	export let tableData: GatherOrderInteface[] = [];
+	export let tableData: Order[] = [],
+			 tab3: boolean;
+	let checkedOrders: Set<any>;
+
+	let checkAll: boolean = false,
+		checks: boolean[] = [];
+	let loading = false,
+		error: string;
+
+	function selectAllRow() {
+		if (checkAll) {
+			checks = new Array(tableData.length).fill(true);
+			checkedOrders = new Set(tableData.map((td) => td.orderId));
+		} else {
+			checks = new Array(tableData.length).fill(false);
+			checkedOrders = new Set();
+		}
+	}
+
+	function onSelectOrder(i: number, row: Order) {
+		if (checks[i] == true) checkedOrders.add(row.orderId);
+		else checkedOrders.delete(row.orderId);
+		checkedOrders = checkedOrders;
+		if (checks.every((v) => v == true) && checks.length == tableData.length) checkAll = true;
+		if (checks[i] == false && checkAll == true) checkAll = false;
+	}
 </script>
 
 <div class="table-container !rounded-b-none !rounded-md h-full">
 	<table class="table table-hover overflow-auto !bg-transparent !rounded-none" class:h-full={tableData.length == 0}>
-		<thead class="!bg-[#fff]">
+		<thead class="!bg-white relative z-10">
 			<tr>
-				<th>STT</th>
-				<th>Mã điểm</th>
-				<th>Tên điểm</th>
-				<th>SĐT liên lạc</th>
-				<th>Trưởng điểm</th>
-				<th>Địa chỉ</th>
-
-				<th>Thao tác</th>
+				<th>
+					<div class="flex items-center gap-2">
+						<input
+							class="dui-checkbox dui-checkbox-secondary dui-checkbox-sm rounded-sm border-[#000]"
+							type="checkbox"
+							bind:checked={checkAll}
+							on:change={selectAllRow}
+						/>
+						STT
+					</div>
+				</th>
+				<th>Mã đơn hàng</th>
+				<th>Loại hàng</th>
+				<th>Cước phí</th>
+				<th>Ngày tạo</th>
+				{#if tab3 == false}
+					<th>Thao tác</th>	
+				{/if}
 			</tr>
 		</thead>
 		{#if tableData.length == 0}
@@ -29,22 +64,35 @@
 		{:else}
 			<tbody>
 				{#each tableData as row, i}
-					<tr>
-						<td>{i + 1}</td>
-						<td>Tesst</td>
-						<td>Tesst</td>
-						<td>Tesst</td>
-						<td>Tesst</td>
-						<td>Tesst</td>
-
-						<td class="flex items-center gap-3">
-							<button type="button" class="btn-icon variant-filled h-8 w-8">
-								<PencilLine size="16" />
-							</button>
-							<button type="button" class="btn-icon variant-filled h-8 w-8">
-								<Trash2 size="16" />
-							</button>
+					<tr class:row-selected={checks[i] == true}>
+						<td class="flex items-center gap-2">
+							<input
+								class="dui-checkbox dui-checkbox-secondary dui-checkbox-sm rounded-sm border-[#000]"
+								type="checkbox"
+								bind:checked={checks[i]}
+								on:change={() => onSelectOrder(i, row)}
+							/>
+							{i + 1}
 						</td>
+						<td>{row.orderId}</td>
+						<td>Loại hàng</td>
+						<td>999</td>
+						<td>
+							date
+						</td>
+						{#if tab3 == false}
+							<td class="flex items-center gap-3">	
+								<div class="dui-tooltip dui-tooltip-bottom" data-tip="Chuyển">
+									<button
+									type="button"
+									class="btn-icon variant-filled h-8 w-8"
+									data-tip="Xóa"
+								>
+									<ArrowRightFromLine size="16" />
+								</button>
+								</div>
+							</td>
+						{/if}
 					</tr>
 				{/each}
 			</tbody>
