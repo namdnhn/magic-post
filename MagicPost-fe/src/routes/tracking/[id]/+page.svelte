ed2244251add4fd18b3dad0e1e139a5837bab399
MinhKhoiUET <minhkhoi281103@gmail.com>
2023-12-29 11:31:52 +0700

    add some code line :))

--- MagicPost-fe/src/routes/tracking/[id]/+page.svelte
+++ MagicPost-fe/src/routes/tracking/[id]/+page.svelte
@@ -5,16 +5,25 @@
 	import Loading from 'src/components/Loading.svelte';
     import { Check} from 'lucide-svelte';
 	export let data: PageServerData;
+	import { goto } from '$app/navigation';
 	console.log('🚀 ~ file: +page.svelte:8 ~ data:', data);
+
+	let trackingOrder: string;
 </script>
 
 <div class="flex flex-col items-center w-full md:my-10">
 	<main class="flex flex-col gap-5 bg-surface-200 md:w-[60vw] p-3">
 
-			<div class="w-full rounded-md flex gap-5">
-				<input type="text" placeholder="Mã đơn" class="dui-input dui-input-bordered dui-input-lg w-full" />
-				<button type="button" class="btn variant-filled-secondary rounded-md md:w-1/5">Theo dõi</button>
-			</div>
+		<div class="w-full  bg-surface-200 rounded-md flex gap-5">
+            <input type="text" placeholder="Mã đơn" 
+            class="dui-input dui-input-bordered dui-input-lg w-full"
+            bind:value={trackingOrder}
+            />
+            <button type="button" 
+            class="btn variant-filled-secondary rounded-md w-1/5"
+            on:click={() => goto(`/tracking/${trackingOrder}`)}
+            >Theo dõi</button>
+        </div>
 			<div class="flex flex-col w-full md:flex-row px-1">
 				<div class="">
 					<p class="text-secondary-500">Mã phiếu gửi</p>
